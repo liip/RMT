24e74c26019883eb38e8decc1a85fc95b09721da
Jeanmonod David <david.jeanmonod@gmail.com>
2012-09-03 21:28:08 +0200

    Configure the project to be run standalone

--- autoload.php
+++ autoload.php
@@ -1,4 +1,16 @@
 <?php
-// Use the composer autoloader and manually add the Liip namespace
-$loader = require_once __DIR__ . '/../../autoload.php';
+
+// Project context autoloader
+if (file_exists(__DIR__.'/../../autoload.php')) {
+    $loader = require_once __DIR__.'/../../autoload.php';
+}
+// Standalone autoloader
+elseif ( file_exists( __DIR__.'/vendor/autoload.php')) {
+    $loader = require_once __DIR__.'/vendor/autoload.php';
+}
+else {
+    throw new Exception("Unable to find the composer autoloader");
+}
+
+// and manually add the Liip namespace, until we move the project on packagist
 $loader->add('Liip', __DIR__);
