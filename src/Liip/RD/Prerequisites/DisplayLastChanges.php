a90007c5fc25ce7485db15066abc003afa2c14b8
Jeanmonod David <david.jeanmonod@gmail.com>
2012-09-20 20:49:09 +0200

    Configure the sandbox to play the dotcom scenario and fix the DisplayLastCommit accordingly

--- src/Liip/RD/Prerequisites/DisplayLastChanges.php
+++ src/Liip/RD/Prerequisites/DisplayLastChanges.php
@@ -11,7 +11,9 @@ class DisplayLastChanges
         $context->getService('output')->writeln(">>>");
         $context->getService('output')->writeln(
             $context->getService('vcs')->getAllModificationsSince(
-                $context->getService('version-persister')->getCurrentVersionTag()
+                $context->getService('vcs')->getTagFromVersion(
+                    $context->getService('version-persister')->getCurrentVersion()
+                )
             )
         );
         $context->getService('output')->writeln("<<<");
