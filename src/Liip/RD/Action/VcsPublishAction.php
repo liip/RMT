5105ec2a3b29d6a9d2acf11e51068ec68e4fc028
Jeanmonod David <david.jeanmonod@gmail.com>
2012-11-10 11:02:43 +0100

    Add a comfirmation on the auto publish action

--- src/Liip/RD/Action/VcsPublishAction.php
+++ src/Liip/RD/Action/VcsPublishAction.php
@@ -2,16 +2,30 @@
 
 namespace Liip\RD\Action;
 
+use Liip\RD\Information\InformationRequest;
+
 class VcsPublishAction extends BaseAction
 {
     public function execute($context)
     {
         $context->getService('vcs')->publishTag(
-            $context->getService('vcs')->getTagFromVersion(
+            $context->getService('version-persister')->getTagFromVersion(
                 $context->getParam('new-version')
             )
         );
         $context->getService('vcs')->publishChanges();
         $this->confirmSuccess($context);
+    }
+
+    public function getInformationRequests()
+    {
+        return array(
+            new InformationRequest('auto-publish', array(
+                'description' => 'Automatically publish the change',
+                'type' => 'boolean',
+                'interactive' => true,
+                'default' => false
+            ))
+        );
     }
 }
