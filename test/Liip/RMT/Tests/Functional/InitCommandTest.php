1ff971b813db46a256b035651e84badc0a12e4c4
Jeanmonod David <david.jeanmonod@gmail.com>
2013-12-13 12:25:04 +0100

    Fix tests for the new config format used in the init command

--- test/Liip/RMT/Tests/Functional/InitCommandTest.php
+++ test/Liip/RMT/Tests/Functional/InitCommandTest.php
@@ -12,18 +12,22 @@ class InitCommandTest extends RMTFunctionalTestBase
         $configFile = '.rmt.yml';
         unlink($configFile);
         $this->assertFileNotExists($configFile);
-//        $this->manualDebug();
         exec('./RMT init --vcs=git --generator=semantic-versioning --persister=vcs-tag -n');
+
+//        $this->manualDebug();
+
         $this->assertFileExists($configFile);
         $config = Yaml::parse(file_get_contents($configFile), true);
-        $masterConfig = $config["branch-specific"]["master"];
 
-        $this->assertEquals('git', $config['vcs']);
+        $defaultConfig = $config["_default"];
+        $masterConfig = $config["master"];
 
-        $this->assertEquals('simple', $config['version-generator']);
+        $this->assertEquals('git', $defaultConfig['vcs']);
+
+        $this->assertEquals('simple', $defaultConfig['version-generator']);
         $this->assertEquals('semantic', $masterConfig['version-generator']);
 
-        $this->assertEquals(array("vcs-tag"=>array("tag-prefix"=>"{branch-name}_")), $config['version-persister']);
+        $this->assertEquals(array("vcs-tag"=>array("tag-prefix"=>"{branch-name}_")), $defaultConfig['version-persister']);
         $this->assertEquals(array("vcs-tag"=>array("tag-prefix"=>"")), $masterConfig['version-persister']);
     }
 }
