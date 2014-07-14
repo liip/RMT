fa3d07e1665f592b99eb8e3cb6691e6427e14fa2
ahilles107 <mikolajczuk.private@gmail.com>
2014-07-14 12:35:19 +0200

    provide a way to add label for release
    beta_releases
    
    add missing option

--- test/Liip/RMT/Tests/Unit/Version/Generator/SemanticGeneratorTest.php
+++ test/Liip/RMT/Tests/Unit/Version/Generator/SemanticGeneratorTest.php
@@ -17,12 +17,17 @@ class SemanticGeneratorTest extends \PHPUnit_Framework_TestCase
     /**
      * @dataProvider getVersionValues
      */
-    public function testIncrement($current, $type, $result)
+    public function testIncrement($current, $type, $label, $result)
     {
         $options = array();
         if ($type){
             $options['type'] = $type;
         }
+
+        if ($label) {
+            $options['label'] = $label;
+        }
+
         $generator = new \Liip\RMT\Version\Generator\SemanticGenerator();
         $this->assertEquals($result, $generator->generateNextVersion($current, $options));
     }
@@ -30,12 +35,18 @@ class SemanticGeneratorTest extends \PHPUnit_Framework_TestCase
     public function getVersionValues()
     {
         return array(
-            array('1.0.0',  'patch', '1.0.1'),
-            array('1.23.0', 'minor', '1.24.0'),
-            array('1.1.19', 'minor', '1.2.0'),
-            array('1.0.0',  'major',  '2.0.0'),
-            array('1.19.3', 'major',  '2.0.0'),
-            array('3.3.3',  'major',  '4.0.0')
+            array('1.0.0',  'patch', 'none', '1.0.1'),
+            array('1.23.0', 'minor', 'none', '1.24.0'),
+            array('1.1.19', 'minor', 'none', '1.2.0'),
+            array('1.0.0',  'major', 'none', '2.0.0'),
+            array('1.19.3', 'major', 'none', '2.0.0'),
+            array('3.3.3',  'major', 'none', '4.0.0'),
+            array('3.3.3',  'major', 'alpha', '4.0.0-alpha'),
+            array('4.0.0-aplha2',  'major', 'beta', '4.0.0-beta'),
+            array('3.3.3',  'minor', 'beta', '3.4.0-beta'),
+            array('4.0.0-beta',  'minor', 'beta', '4.0.0-beta2'),
+            array('4.0.0',  'minor', 'rc', '4.1.0-rc'),
+            array('4.0.0-rc',  'minor', 'none', '4.0.0'),
         );
     }
 
@@ -46,16 +57,21 @@ class SemanticGeneratorTest extends \PHPUnit_Framework_TestCase
     public function testIncrementWithInvalidType()
     {
         $generator = new \Liip\RMT\Version\Generator\SemanticGenerator();
-        $generator->generateNextVersion('1.0.0', array('type'=>'full'));
+        $generator->generateNextVersion('1.0.0', array('type'=>'full', 'label'=>'none'));
     }
 
     public function testCompare()
     {
         $generator = new \Liip\RMT\Version\Generator\SemanticGenerator();
         $this->assertEquals(-1, $generator->compareTwoVersions('1.0.0', '1.0.1'));
+        $this->assertEquals(-1, $generator->compareTwoVersions('1.0.0-beta', '1.0.0'));
         $this->assertEquals(0, $generator->compareTwoVersions('1.0.0', '1.0.0'));
         $this->assertEquals(1, $generator->compareTwoVersions('1.0.1', '1.0.0'));
         $this->assertEquals(1, $generator->compareTwoVersions('1.0.11', '1.0.1'));
+        $this->assertEquals(1, $generator->compareTwoVersions('1.0.1', '1.0.1-alpha'));
+        $this->assertEquals(1, $generator->compareTwoVersions('1.0.1-beta', '1.0.1-alpha'));
+        $this->assertEquals(1, $generator->compareTwoVersions('1.0.11-rc', '1.0.1-beta'));
+        $this->assertEquals(1, $generator->compareTwoVersions('1.0.2', '1.0.1-rc'));
     }
 
 }
